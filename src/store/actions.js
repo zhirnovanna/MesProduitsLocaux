1a77450a30f0b0a7b3476e6a239dc4f6a0969e8d
Apple <jade.lecuyer@epitech.eu>
2021-04-05 15:40:29 +0200

    upgrade search function: cancel previous fetch

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,7 @@
 import { ENDPOINT } from './index';
 
+let abortControllerProduct = new AbortController();
+let abortControllerCount = new AbortController();
 
 export const actions = {
     async getAllProducts ({ commit }) {
@@ -18,49 +20,75 @@ export const actions = {
     },
 
     async getFilteredProducts ({ commit }, parameters) {
-        // get a filtered set of products
-        // possible parameters are region, category, max_price, min_price, search, sorting, offset, limit
-        // pass empty strings for unneeded parameters
-        let response = await fetch(ENDPOINT + 'products?region=' + parameters.filters.region 
-                                    + '&category=' + parameters.filters.category
-                                    + '&min_price=' + parameters.filters.min_price
-                                    + '&max_price=' + parameters.filters.max_price
-                                    + '&search=' + parameters.filters.search
-                                    + '&sorting=' + parameters.filters.sorting
-                                    + '&offset=' + parameters.offset
-                                    + '&limit=' + parameters.limit);
+        abortControllerProduct.abort(); // Cancel the previous request
+        abortControllerProduct = new AbortController();
 
-        if (!response.ok) {
-            const message = `An error has occured: ${response.status}`;
-            console.log(message);
-            return null;
+        try {
+            // get a filtered set of products
+            // possible parameters are region, category, max_price, min_price, search, sorting, offset, limit
+            // pass empty strings for unneeded parameters
+            let response = await fetch(ENDPOINT + 'products?region=' + parameters.filters.region 
+                                        + '&category=' + parameters.filters.category
+                                        + '&min_price=' + parameters.filters.min_price
+                                        + '&max_price=' + parameters.filters.max_price
+                                        + '&search=' + parameters.filters.search
+                                        + '&sorting=' + parameters.filters.sorting
+                                        + '&offset=' + parameters.offset
+                                        + '&limit=' + parameters.limit,
+                                        { signal: abortControllerProduct.signal });
+
+            if (!response.ok) {
+                const message = `An error has occured: ${response.status}`;
+                console.log(message);
+                return null;
+            }
+
+            const data = await response.json();
+
+            commit('SET_PRODUCTS', data);
         }
-
-        const data = await response.json();
-
-        commit('SET_PRODUCTS', data);
+        catch (ex) {
+            if (ex.name === 'AbortError') {
+            return; // Continuation logic has already been skipped, so return normally
+            }
+        
+            throw ex;
+        }
     },
 
     async getMatchedProductsNumber ({ commit }, parameters = {'region': '', 'category': '', 'min_price': '', 'max_price': '', 'search': ''}) {
-        // get a filtered set of products total number
-        // if no parameters get total number of products
-        // possible parameters are region, category, max_price, min_price, search
-        // empty strings for unneeded parameters
-        let response = await fetch(ENDPOINT + 'products/count?region=' + parameters.region 
-                                    + '&category=' + parameters.category
-                                    + '&min_price=' + parameters.min_price
-                                    + '&max_price=' + parameters.max_price
-                                    + '&search=' + parameters.search);
+        abortControllerCount.abort(); // Cancel the previous request
+        abortControllerCount = new AbortController();
 
-        if (!response.ok) {
-            const message = `An error has occured: ${response.status}`;
-            console.log(message);
-            return null;
+        try {
+            // get a filtered set of products total number
+            // if no parameters get total number of products
+            // possible parameters are region, category, max_price, min_price, search
+            // empty strings for unneeded parameters
+            let response = await fetch(ENDPOINT + 'products/count?region=' + parameters.region 
+                                        + '&category=' + parameters.category
+                                        + '&min_price=' + parameters.min_price
+                                        + '&max_price=' + parameters.max_price
+                                        + '&search=' + parameters.search,
+                                        { signal: abortControllerCount.signal });
+
+            if (!response.ok) {
+                const message = `An error has occured: ${response.status}`;
+                console.log(message);
+                return null;
+            }
+
+            const data = await response.json();
+
+            commit('SET_PRODUCTS_NUMBER', data);
         }
-
-        const data = await response.json();
-
-        commit('SET_PRODUCTS_NUMBER', data);
+        catch (ex) {
+            if (ex.name === 'AbortError') {
+            return; // Continuation logic has already been skipped, so return normally
+            }
+        
+            throw ex;
+        }
     },
 
     async getProduct ({ commit }, productId) {
